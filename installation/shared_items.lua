375622e7f8d36ef18cb4659c5abc5f3f599f3776
RexshackGaming <rexshackuk@gmail.com>
2024-07-04 14:54:00 +0100

    readme update

--- installation/shared_items.lua
+++
@@ -1,2 +0,0 @@
-    -- fish monger
-    ["raw_fish"] = {["name"] = "raw_fish", ["label"] = "Raw Fish", ["weight"] = 300, ["type"] = "item",	["image"] = "raw_fish.png", ['unique'] = false, ['useable'] = true, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ["description"] = "ready for cooking"},
